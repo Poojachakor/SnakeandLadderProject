f727dfd3c260414d3fb1b4f6b5109dcfa2cc1a39
Poojachakor <chakorpooja07@gmail.com>
2023-06-09 19:20:29 +0530

    Add: SnakeAndLadder

---
+++ SnakeAndLadder/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace SnakeAndLadder
+{
+    internal class Program
+    {
+        public const int START_POINT = 0;
+        public const int END_POINT = 100;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to snakes and ladders game \nEnter player name");
+            string player1 = Console.ReadLine();
+            int playerPosition = START_POINT;
+            Console.WriteLine("Current Position : " + playerPosition);
+        }
+    }
+}
